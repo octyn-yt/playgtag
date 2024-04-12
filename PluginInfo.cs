d43057c9ed9361c1b034ae5055ff0f22a520fe21
Jes <141727697+octyn-yt@users.noreply.github.com>
2024-04-12 22:55:19 +0100

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -1,12 +1,12 @@
-﻿namespace dontplaygtag
+﻿namespace playgtag
 {
 	/// <summary>
 	/// This class is used to provide information about your mod to BepInEx.
 	/// </summary>
 	class PluginInfo
 	{
-		public const string GUID = "com.xenon.gorillatag.dontplaygtag";
-		public const string Name = "dontplaygtag";
+		public const string GUID = "com.jes.gorillatag.playgtag";
+		public const string Name = "playgtag";
 		public const string Version = "1.0.0";
 	}
 }
